98ccdc27eb8b0e81242cc84af50f8f4cc61b05b2
kunxin-chor <chorkunxin@yahoo.com>
2020-11-05 15:43:25 +0000

    Removed solution

--- q1/script.js
+++ q1/script.js
@@ -1,7 +1,4 @@
 // write your answer here
-let header = document.querySelector('h1');
-let important = document.querySelector('span#important');
-let todo = document.querySelector('li.todo')
 
 // LEAVE THE BELOW ALONE! DON'T TOUCH
 try {
