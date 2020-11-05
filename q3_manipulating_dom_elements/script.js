15bc3f25d937265d2b17025bef6b36feed17eab9
kunxin-chor <chorkunxin@yahoo.com>
2020-11-05 17:26:04 +0000

    Added q4

---
+++ q3_manipulating_dom_elements/script.js
@@ -0,0 +1,7 @@
+// PLACE YOUR CODE HERE
+
+
+// LEAVE THE BELOW ALONE! DON'T TOUCH
+try {
+   module.exports = exports = {header, important, todo};
+} catch (e) {}
---
+++ q3_manipulating_dom_elements/solutions.js
@@ -0,0 +1,10 @@
+// write your answer here
+document.querySelector('h1').innerText = "Hello world!";
+document.querySelector('span#important').style.backgroundColor = 'red';
+document.querySelector('li.todo').style.fontFamily = "Verdana";
+document.querySelector('li.todo').style.fontSize = "16px";
+
+// LEAVE THE BELOW ALONE! DON'T TOUCH
+try {
+   module.exports = exports = {header, important, todo};
+} catch (e) {}
