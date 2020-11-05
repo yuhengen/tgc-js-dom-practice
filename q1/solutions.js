c656eaadc32a10d1ba9da89e5fff5b3fdee85df6
kunxin-chor <chorkunxin@yahoo.com>
2020-11-05 16:00:34 +0000

    Added solution hack

---
+++ q1/solutions.js
@@ -0,0 +1,9 @@
+// write your answer here
+let header = document.querySelector('h1');
+let important = document.querySelector('span#important');
+let todo = document.querySelector('li.todo')
+
+// LEAVE THE BELOW ALONE! DON'T TOUCH
+try {
+   module.exports = exports = {header, important, todo};
+} catch (e) {}
