2246579c652b7047d5f258125f6ebe003387b6a3
Kun <extrakun@yahoo.com.sg>
2020-11-09 08:25:29 +0000

    merged

--- q4_more_manipulation/script.js
+++ q4_more_manipulation/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.querySelector('h1').style.color = 'green';
 
 document.querySelector('li.finished').innerText = "Repay credit card debt";
@@ -18,6 +17,3 @@ emphasis.style.lineHeight = "24px";
 document.querySelector('li#urgent span.todo').style.backgroundColor = 'yellow';
 
 document.querySelector('h2').innerHTML = "<span class='greetings'>About Us</span>";
-=======
-// PLACE YOUR CODE HERE
->>>>>>> upstream/main
