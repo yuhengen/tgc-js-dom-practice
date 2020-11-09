aaec47d4ba875eff5b4ee849bb85604a0ae0302d
kunxin-chor <chorkunxin@yahoo.com>
2020-11-09 08:56:55 +0000

    Added more qns

--- r2q1_Rounding_up_the_criminals/solutions.js
+++
@@ -1,8 +0,0 @@
-const classAelements = document.querySelectorAll(".a");
-classAelements.forEach((element) => (element.style.backgroundColor = "red"));
-const undercoverArrestedElements = document.querySelectorAll(
-  "div.undercover.a"
-);
-undercoverArrestedElements.forEach(
-  (element) => (element.style.backgroundColor = "yellow")
-);
